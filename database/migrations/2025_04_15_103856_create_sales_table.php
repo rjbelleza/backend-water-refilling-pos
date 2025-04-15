b08b7dd66e2755e358f94803a38274347745ba7d
rjbelleza <rjbelleza19@gmail.com>
2025-04-15 18:49:31 +0800

    Migrated Table Schemas

---
+++ database/migrations/2025_04_15_103856_create_sales_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->string('customer', 50)->default('walk-in');
+            $table->double('total_amount');
+            $table->timestamp('created_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sales');
+    }
+};
