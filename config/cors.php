d38be1db163965ec74e4ad4aabb1dcbb2564fb83
rjbelleza <rjbelleza19@gmail.com>
2025-06-23 08:26:59 +0800

    Update: Allowed origins

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://aquasprings.vercel.app'],
+    'allowed_origins' => ['https://aquasprings.vercel.app', 'http://localhost:5173'],
 
     'allowed_origins_patterns' => [],
 
